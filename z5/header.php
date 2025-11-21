8f61cca05701156993314274b98a99f30acb72e1
Damian Skonieczny <naimad2003@wp.pl>
2025-11-21 10:55:46 +0100

    lab5 init

---
+++ z5/header.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1); ?>
+<header>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top mt-0 mb-0 ms-0 me-0 pt-0 pb-0 ps-0 pe-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php?page=home">Z4 Monitor</a>
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="navbar-nav me-auto">
+                        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="index.php?page=home">Monitoruj</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?page=dodaj">Dodaj Host</a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+
+                    <ul class="navbar-nav ms-auto">
+                        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" id="mute-button" title="Włącz alarmy">
+                                    <i class="bi bi-volume-mute-fill"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" data-bs-toggle="dropdown">
+                                    <?php
+                                    $default_avatar = 'media/menu_icons/info.svg';
+                                    if (!empty($_SESSION['avatar_path'])) {
+                                        $avatar_src = '../' . htmlspecialchars($_SESSION['avatar_path']);
+                                    } else { $avatar_src = $default_avatar; }
+                                    ?>
+                                    <img src="<?php echo $avatar_src; ?>" alt="Avatar" class="rounded-circle" height="24" width="24" style="margin-right: 8px; object-fit: cover;">
+                                    <?php echo htmlspecialchars($_SESSION['username']); ?>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li>
+                                        <a class="dropdown-item" href="wyloguj.php">
+                                            <i class="bi bi-box-arrow-right me-2"></i>Wyloguj się
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        <?php else: ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?page=logowanie">Zaloguj się</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?page=rejestracja">Zarejestruj się</a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ z5/pages/logowanie.php
@@ -0,0 +1,48 @@
+<div class="container" style="max-width: 400px;">
+    <main class="form-signin w-100 m-auto text-center">
+        <form action="../src/login_handler.php" method="post">
+            <img class="mb-4" src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+
+            <h1 class="h3 mb-3 fw-normal">Zaloguj się (Bezpiecznie)</h1>
+
+            <div class="form-floating mb-2">
+                <input type="text" class="form-control" id="username" name="username" placeholder="Nazwa użytkownika" required>
+                <label for="username">Nazwa użytkownika</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Hasło" required>
+                <label for="password">Hasło</label>
+            </div>
+
+            <input type="hidden" id="screen_res" name="screen_res" value="">
+            <input type="hidden" id="window_res" name="window_res" value="">
+            <input type="hidden" id="colors" name="colors" value="">
+            <input type="hidden" id="cookies_enabled" name="cookies_enabled" value="">
+            <input type="hidden" id="java_enabled" name="java_enabled" value="">
+            <input type="hidden" id="language" name="language" value="">
+
+            <input type="hidden" name="redirect_url" value="../z4/index.php?page=home">
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Zaloguj się</button>
+
+            <p class="mt-3 mb-1">
+                Nie masz konta? <a href="index.php?page=rejestracja">Zarejestruj się</a>
+            </p>
+        </form>
+    </main>
+    <script>
+        // Ten skrypt uruchomi się automatycznie po załadowaniu strony
+        (function() {
+            // Zbieramy dane o ekranie
+            document.getElementById('screen_res').value = screen.width + 'x' + screen.height;
+            document.getElementById('window_res').value = window.innerWidth + 'x' + window.innerHeight;
+            document.getElementById('colors').value = screen.colorDepth;
+
+            // Zbieramy dane o przeglądarce
+            document.getElementById('cookies_enabled').value = navigator.cookieEnabled ? 'Tak' : 'Nie';
+            // metoda .javaEnabled() została oznaczona przez IDE jako "deprecated"
+            document.getElementById('java_enabled').value = navigator.javaEnabled() ? 'Tak' : 'Nie';
+            document.getElementById('language').value = navigator.language || navigator.userLanguage;
+        })();
+    </script>
+</div>
