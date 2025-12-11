cb88ee53f43e74d21a85663086d7d574b583b9da
Damian Skonieczny <naimad2003@wp.pl>
2025-12-11 02:29:12 +0100

    Netflix fix 1 uploads

--- z6b/scripts/upload_films.php
+++ z6b/scripts/upload_films.php
@@ -18,11 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['film_file'])) {
     // Auto-naprawa folderu
     if (!is_dir($upload_dir)) mkdir($upload_dir, 0777, true);
     // Unikalna nazwa pliku
-    $file_name = time() . '_' . basename($_FILES['films_file']['name']);
+    $file_name = time() . '_' . basename($_FILES['film_file']['name']);
     $target_file = $upload_dir . $file_name;
 
     // Przesyłanie
-    if (move_uploaded_file($_FILES['music_file']['tmp_name'], $target_file)) {
+    if (move_uploaded_file($_FILES['film_file']['tmp_name'], $target_file)) {
         // SQL Insert
         $sql = "INSERT INTO film (title, director, idu, filename, idft) VALUES (?, ?, ?, ?, ?)";
         $stmt = $pdo->prepare($sql);
