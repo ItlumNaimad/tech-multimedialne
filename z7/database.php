56df806bea7aacbf5b2c37f87ac6ac9ec835cff5
Damian Skonieczny <naimad2003@wp.pl>
2026-01-15 11:11:51 +0100

    Zmiany
    Poprawa łączenia z bazą danych

--- z7/database.php
+++ z7/database.php
@@ -1,17 +1,17 @@
 <?php
-
+// z7/database.php - WERSJA DLA MYSQLI (Zadanie 7)
 $host = '127.0.0.1';
 $db_name = 'damskopb_z7';
-$username = '#####';
-$password = '#####';
-$charset = 'utf8mb4';
+$username = 'damskopb_...';
+$password = '...';
 
-$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+// Uwaga: Tutaj używamy mysqli_connect, a nie PDO!
+$polaczenie = mysqli_connect($host, $username, $password, $db_name);
 
-try {
-    $pdo = new PDO($dsn, $username, $password, $options);
-} catch (PDOException $e) {
-    die("Błąd połączenia z bazą: " . $e->getMessage());
+if (!$polaczenie) {
+    die("Błąd połączenia z MySQL: " . mysqli_connect_error());
 }
 
+// Ustawienie kodowania znaków
+mysqli_set_charset($polaczenie, "utf8");
+?>
--- z7/dodaj.php
+++ z7/dodaj.php
@@ -1,7 +1,23 @@
 <?php
+// Włączamy raportowanie błędów (Diagnostyka błędu 500)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Sprawdzamy czy plik bazy istnieje
+if (!file_exists('database.php')) {
+    die("Błąd: Nie znaleziono pliku database.php w folderze " . __DIR__);
+}
+
 require_once 'database.php';
 
+// Sprawdzamy czy połączenie zostało utworzone (czy zmienna z database.php istnieje)
+if (!isset($polaczenie)) {
+    die("Błąd krytyczny: Zmienna \$polaczenie nie istnieje. Sprawdź czy plik database.php używa mysqli_connect!");
+}
+
 if (!empty($_POST['text1'])) {
+    // Teraz to powinno zadziałać, jeśli $polaczenie jest poprawne
     $text = mysqli_real_escape_string($polaczenie, $_POST['text1']);
 
     $sql = "INSERT INTO ajax_from_db (text1) VALUES ('$text')";
@@ -9,7 +25,9 @@ if (!empty($_POST['text1'])) {
     if (mysqli_query($polaczenie, $sql)) {
         echo "Dodano: $text. Możesz zamknąć to okno.";
     } else {
-        echo "Błąd: " . mysqli_error($polaczenie);
+        echo "Błąd SQL: " . mysqli_error($polaczenie);
     }
+} else {
+    echo "Błąd: Puste pole text1.";
 }
 ?>
